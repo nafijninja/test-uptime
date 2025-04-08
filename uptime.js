2dd199a2e6549d4e34bdfd33b15bfe081177ec09
NAFIJ <168545572+nafijninja@users.noreply.github.com>
2025-04-08 21:35:24 +0600

    Update uptime.js

--- uptime.js
+++ uptime.js
@@ -1,10 +1,11 @@
-// 
-const targetUrl = "https://jsonplaceholder.typicode.com/todos/1"; // Example URL
+// uptime.js
+
+const targetUrl = "https://jsonplaceholder.typicode.com/todos/1";  // Example URL
 
 // Function to send request and check server status
 async function sendRequest() {
   try {
-    const response = await axios.get(targetUrl);
+    const response = await axios.get(targetUrl);  // Make the request using Axios
     if (response.status >= 200 && response.status < 300) {
       return { message: `Server is Online (Status: ${response.status})`, statusClass: 'online' };
     } else {
